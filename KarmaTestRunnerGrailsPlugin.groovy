91baaf11d43558a4f6217675dcd1e1ec6de8186c
Florian Grundig <florian.grundig@gmx.de>
2013-09-10 09:45:12 +0200

    updated plugin description

--- KarmaTestRunnerGrailsPlugin.groovy
+++ KarmaTestRunnerGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class KarmaTestRunnerGrailsPlugin {
-    def version = "0.1-SNAPSHOT"
+    def version = "0.1.1-SNAPSHOT"
     def grailsVersion = "2.0 > *"
     def pluginExcludes = [
         "src/docs/**/*",
@@ -7,13 +7,13 @@ class KarmaTestRunnerGrailsPlugin {
     ]
 
     def title = "Karma Test Runner Plugin"
-    def author = "Florian Grundig "
+    def author = "Florian Grundig"
     def authorEmail = "florian.grundig@gmx.de"
-    def description = 'Runs javascript unit or e2e tests with karma. Provides a test type "javascript" which can be run in any test phase.'
+    def description = 'Runs javascript unit or e2e tests with karma. Provides a test type \"javascript\" which can be run in any test phase.'
     def documentation = "https://github.com/ImmobilienScout24/karma-test-runner"
 
     def license = "APACHE"
     def organization = [ name: "ImmobilienScout24", url: "http://www.immobilienscout24.de" ]
-    def issueManagement = [ system: "GITHUB", url: "https://github.com/ImmobilienScout24/karma-test-runner/issues" ]
+    def issueManagement = [ system: "Github", url: "https://github.com/ImmobilienScout24/karma-test-runner/issues" ]
     def scm = [ url: "https://github.com/ImmobilienScout24/karma-test-runner" ]
 }
