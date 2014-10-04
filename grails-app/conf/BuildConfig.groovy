f557aa5b48dce27f9b936f1784305cc6d1e7ee0f
Florian Grundig <florian.grundig@gmx.de>
2014-10-04 15:16:37 +0200

    using junit-karma-testrunner v1.4 which supports java 1.6

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -12,7 +12,7 @@ grails.project.dependency.resolution = {
 	}
 
 	dependencies {
-		compile 'de.is24.util:junit-karma-testrunner:1.3'
+		compile 'de.is24.util:junit-karma-testrunner:1.4'
 	}
 
 	plugins {
