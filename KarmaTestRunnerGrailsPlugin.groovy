1f05f546f45d58ceccfb0365c322f037107f0aed
Florian Grundig <florian.grundig@gmx.de>
2014-10-04 15:35:49 +0200

    v0.2.2

--- KarmaTestRunnerGrailsPlugin.groovy
+++ KarmaTestRunnerGrailsPlugin.groovy
@@ -1,5 +1,5 @@
 class KarmaTestRunnerGrailsPlugin {
-    def version = "0.2.2-SNAPSHOT"
+    def version = "0.2.2"
     def grailsVersion = "2.0 > *"
     def pluginExcludes = [
         "src/docs/**/*",
